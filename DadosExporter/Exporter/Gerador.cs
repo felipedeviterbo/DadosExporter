d48ac093f0d56e11c782a388625067ee72d1bdbe
felipedeviterbo <44898703+felipedeviterbo@users.noreply.github.com>
2019-01-24 00:34:30 -0200

    Initial

---
+++ DadosExporter/Exporter/Gerador.cs
@@ -0,0 +1,70 @@
+﻿using DadosExporter.Models;
+using log4net;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DadosExporter.Exporter
+{
+    public class Gerador
+    {
+        private static readonly ILog Log =
+              LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public void GerarRelatorio(Report report)
+        {
+            if (report == null) { Log.Error("Tentativa de gerar relatório inválido."); return; }
+            if (string.IsNullOrEmpty(report.FilePath))
+            {
+                Log.Error($"Relatório {report.Name} sem arquivo de sql de origem informado");
+                return;
+            }
+            if (!File.Exists(report.FilePath))
+            {
+                Log.Error($"Arquivo {report.FilePath} do relatório {report.Name} não encontrado");
+                return;
+            }
+            if (string.IsNullOrEmpty(report.PathDestino))
+            {
+                Log.Error($"Relatório {report.Name} não possui path de destino informada.");
+            }
+            if (Directory.Exists(report.PathDestino))
+            {
+                Log.Error($"Relatório {report.Name} possui caminho de destino inválido({report.PathDestino}).");
+                return;
+            }
+
+            string sql = ObterSqlArquivo(report.FilePath);
+            GerarArquivoCsv(report, new List<string>());
+            SalvarGeracaoArquivo(report.Name, sql);
+        }
+
+        public string ObterSqlArquivo(string path)
+        {
+            return File.ReadAllText(path);
+        }
+
+        public void GerarArquivoCsv(Report report, IEnumerable<string> dados)
+        {
+            string caminho = string.Format(@"{0}\{1}{2}.csv", report.PathDestino, report.Name, DateTime.Now.ToString("DDMMYYYYHHMMSS"));
+            using (StreamWriter file = new StreamWriter(caminho))
+            {
+                foreach (string line in dados)
+                {
+                    if (!string.IsNullOrEmpty(line))
+                    {
+                        file.WriteLine(line);
+                    }
+                }
+            }
+        }
+        
+        public void SalvarGeracaoArquivo(string nome, string consulta)
+        {
+
+        }
+    }
+}
---
+++ DadosExporter/Services/ExportarDadosService.cs
@@ -0,0 +1,40 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DadosExporter
+{
+    public partial class ExportarDadosService : ServiceBase
+    {
+        private static readonly ILog Log =
+              LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public ExportarDadosService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Log.Info("DadosExporter iniciado");
+        }
+
+        protected override void OnStop()
+        {
+            Log.Info("DadosExporter finalizado");
+        }
+
+        protected override void OnContinue()
+        {
+            Log.Info("DadosExporter ativo");
+        }
+
+    }
+}
